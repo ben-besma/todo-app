55f9dd1629b71a961efe5ffe3203e183a961a1fb
ben-besma <148901852+ben-besma@users.noreply.github.com>
2023-11-03 14:58:52 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div id="app">
+    <todo-app>
+      
+    </todo-app>
+  </div>
+
+  <main>
+    
+    
+  </main>
+</template>
+
